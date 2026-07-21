e539698fa365c4718ea22e7b3845891bde2629b9
KanchiMoe <github@chiyo-chan.me>
2026-07-21 02:02:15 +0100

    add logging line, x of users without a colour role

--- src/discord/colour_check.py
+++ src/discord/colour_check.py
@@ -49,6 +49,7 @@ async def no_colour_check() -> None:
         logging.warning("Channel was not found in cache")
         channel = await guild.fetch_channel(COLOUR_LOG_CHANNEL)
 
+    logging.info(f"There are {len(users_without_colour_roles)} users without a role colour role.")
     await channel.send(content=f"There are **{len(users_without_colour_roles)}** users without a role colour role.")
 
     # assign a random role
