3c330f0f96844bd4a7fa53769d049429ff29c633
KanchiMoe <github@chiyo-chan.me>
2026-08-04 01:42:26 +0100

    add get all member ids

--- src/sql/sql.py
+++ src/sql/sql.py
@@ -30,3 +30,32 @@ def add_run(action: str):
     conn.close()
 
     return None
+
+def get_member_data() -> list:
+    # to do, make this better w/ add_run
+    try:
+        conn = psycopg2.connect()
+
+    except Exception as e:
+        err_msg = f"Could not connect to database: {e}"
+        logging.critical(err_msg)
+        quit()
+        raise RuntimeError(err_msg)
+
+    # get all IDs
+    all_member_ids = []
+
+    with conn.cursor() as cursor:
+        cursor.execute("""
+            SELECT member_id FROM members;
+            """,
+        )
+        # results from db
+        rows = cursor.fetchall()
+        for row in rows:
+            member_id = row[0]
+            all_member_ids.append(member_id)
+
+    logging.info(f"Entries in all_member_ids: {len(all_member_ids)}")
+
+    return all_member_ids
